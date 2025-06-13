b8f528d23e93258a1c8f3b4b76cdfa9de7b06509
Nataly Aparecida Damrat <nataly.damrat@escola.pr.gov.br>
2025-06-13 08:48:03 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+// Exemplo de perguntas
+const perguntas = [
+  {
+    categoria: 'Geografia',
+    pergunta: 'Qual é a capital do Brasil?',
+    resposta: 'Brasília'
+  },
+  {
+    categoria: 'História',
+    pergunta: 'Quem foi o primeiro presidente do Brasil?',
+    resposta: 'Deodoro da Fonseca'
+  },
+  {
+    categoria: 'Matemática',
+    pergunta: 'Quanto é 7 x 8?',
+    resposta: '56'
+  },
+  {
+    categoria: 'Ciências',
+    pergunta: 'Qual é o maior planeta do sistema solar?',
+    resposta: 'Júpiter'
+  }
+];
